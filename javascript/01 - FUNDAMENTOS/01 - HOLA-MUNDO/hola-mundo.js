4c4d81e81a59c59d7c7f4bf377d18ec95f64ae25
advngrajeda <93312415+advngrajeda@users.noreply.github.com>
2026-03-28 14:02:01 -0700

    Create hola-mundo.js
    feat: completar ejercicio 1 hola mundo con 3 console.log separados y comentarios

---
+++ javascript/01 - FUNDAMENTOS/01 - HOLA-MUNDO/hola-mundo.js
@@ -0,0 +1,7 @@
+//Ejercicio 0 - Hola mundo
+//Autor: adan.grajeda
+//Fecha: 28 de marzo de 2026
+
+console.log('Hola mundo! Hoy es 28 de Marzo de 2026.' );
+console.log('Adan Grajeda');
+console.log("Este es el ejercicio numero #0 de mi portafolio 2026");
